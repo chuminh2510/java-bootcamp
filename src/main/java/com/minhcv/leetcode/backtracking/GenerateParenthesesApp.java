2ab687e542faa6fed01eb13e7a9be0ce3528b7ad
Chu Van Minh <chuminhuet@gmail.com>
2022-07-21 17:33:43 +0700

    Generate Parentheses string

--- src/main/java/com/minhcv/leetcode/backtracking/GenerateParenthesesApp.java
+++ src/main/java/com/minhcv/leetcode/backtracking/GenerateParenthesesApp.java
@@ -3,7 +3,6 @@ package com.minhcv.leetcode.backtracking;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Stack;
 
 /**
  * https://leetcode.com/problems/generate-parentheses/
@@ -21,49 +20,22 @@ public class GenerateParenthesesApp {
 
         public List<String> appendParentheses(String s, int countOpen, int countClose) {
             if (countOpen == 0 && countClose == 0) {
-                if (isValidParentheses(s)) {
-                    return Arrays.asList(s);
-                } else {
-                    return null;
-                }
+                return Arrays.asList(s);
             }
             List<String> validParentheses = new ArrayList<>();
             if (countOpen > 0) {
-                int tempCount = countOpen;
-                List<String> ret = appendParentheses(s + "(", --tempCount, countClose);
+                List<String> ret = appendParentheses(s + "(", countOpen - 1, countClose);
                 if (ret != null) {
                     validParentheses.addAll(ret);
                 }
             }
-            if (countClose > countOpen) {
-                int tempCount = countClose;
-                List<String> ret = appendParentheses(s + ")", countOpen, --tempCount);
+            if (countClose > countOpen) { // This ensures parentheses string is always correct
+                List<String> ret = appendParentheses(s + ")", countOpen, countClose - 1);
                 if (ret != null) {
                     validParentheses.addAll(ret);
                 }
             }
             return validParentheses;
-        }
-
-        public boolean isValidParentheses(String s) {
-            if (s.length() == 0) {
-                return false;
-            }
-            Stack<Character> parentheses = new Stack<>();
-            parentheses.push(s.charAt(0));
-            for (int idx = 1; idx < s.length(); idx++) {
-                char c = s.charAt(idx);
-                if (c == ')') {
-                    if (parentheses.size() > 0 && parentheses.peek() == '(') {
-                        parentheses.pop();
-                    } else {
-                        parentheses.push(c);
-                    }
-                } else {
-                    parentheses.push(c);
-                }
-            }
-            return parentheses.size() == 0;
         }
     }
 }
